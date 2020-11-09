9fe6d0743689085a8553ab7248372a342e275fd1
prathibha27 <42810644+prathibha27@users.noreply.github.com>
2020-11-09 09:11:18 -0800

    Delete Area_circle.sh

--- Area_circle.sh
+++
@@ -1,7 +0,0 @@
-echo "To calculate the area of a circle"
-echo "Enter the radius of the circle"
-read r
-area=$(echo "3.14*$r*$r"|bc)
-circumference=$(echo "2*3.14*$r"|bc)
-echo "Area :"$area
-echo "circumference :"$circumference
