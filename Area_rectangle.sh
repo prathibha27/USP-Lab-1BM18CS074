e0e391b433d83d593424bc80a54711566f19da21
prathibha27 <42810644+prathibha27@users.noreply.github.com>
2020-11-09 09:11:52 -0800

    Delete Area_rectangle.sh

--- Area_rectangle.sh
+++
@@ -1,8 +0,0 @@
-echo "To calculate the area and perimeter of a rectangle"
-echo "Enter the length and Breadth"
-read l
-read b
-area=$(echo "$l*$b"|bc)
-perimeter=$(echo "2*($l+$b)"|bc)
-echo "Area of the rectangle:"$area
-echo "Perimeter of rectangle:"$perimeter
